1a64a5b9d1bf0cf2397d15728a86cf6127d734fa
ByteWaliChai <prabhatpeak@gmail.com>
2025-07-11 16:56:34 +0000

    started JS journey — learned about Factory and Factory Functions today!

---
+++ JS/hello.js
@@ -0,0 +1,34 @@
+consol.log('Hello from Chai')
+
+
+
+
+function printChai() {
+    
+    console.log('Hello Chai');
+
+}
+
+
+function bringBrush() {
+    console.log('Hanji, le aaya brush');
+    
+}
+
+function bringBrush2(numberOfBrush) {
+    console.log(`Hanji, le aaya ${numberOfBrush} brush`);
+    
+}
+
+function addTwo(ekNum, doNum) {
+    return ekNum + doNum
+}
+
+
+bringBrush()
+printChai()
+bringBrush2(15)
+addTwo(1, 1)
+
+console.log(addTwo(2, 2));
+
