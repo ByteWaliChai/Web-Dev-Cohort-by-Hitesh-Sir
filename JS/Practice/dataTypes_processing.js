dbfc9419af49e43bbf4aa7c4f21d1996a32c785a
ByteWaliChai <prabhatpeak@gmail.com>
2025-07-13 19:06:06 +0000

    practiced JavaScript data types — strings, numbers, booleans & more

---
+++ JS/Practice/dataTypes_processing.js
@@ -0,0 +1,44 @@
+// Varibles
+
+let admin;
+let name;
+
+name = 'Johan';
+admin = name;
+
+// console.log(admin);
+
+
+// Data Types:
+
+// Number
+let n = 123;
+n = 12.453;
+
+console.log( 1 / 0 );  // Infinity
+console.log( Infinity ); // Infinity
+console.log( "not a number" / 2 ); // NaN, such division is erroneous
+
+
+// BigInt
+let bigInt = 1234567890123456789012345678901234567890n;
+
+
+// String
+let str = "Hello";
+let str2 = 'Single quotes are ok too';
+let phrase = `can embed another ${str}`;
+
+
+// Boolean (logical type)
+let nameFieldChecked = true; // yes, name field is checked
+let ageFieldChecked = false; // no, age field is not checked
+
+
+// Null
+let age = null;
+
+
+// Undefined
+let myName;
+
